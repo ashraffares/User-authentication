90acd4ef98710e60ddefb5813b02472442385fed
ashraffares <ashraffares090@gmail.com>
2021-10-15 15:49:12 +0200

    Add new view and create signup form with form_with

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'main#index'
   get 'sign_up', to: 'registration#new'
+  post 'sign_up', to: 'registration#create'
   get 'about-us', to: 'about#index', as: :about
 end
