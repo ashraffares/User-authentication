ca2372339829a828280049ad19c69cdafc223fa9
ashraffares <ashraffares090@gmail.com>
2021-10-15 12:10:28 +0200

    Add alert partial

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,6 @@
 class MainController < ApplicationController
-  def index; end
+  def index
+    flash[:notice] = 'Loged in successfully'
+    flash[:alert] = 'Invalid email or password'
+  end
 end
