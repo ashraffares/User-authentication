03a5a61a67a9e993a4cf95d8476550ff57224b74
ashraffares <ashraffares090@gmail.com>
2021-10-15 18:38:20 +0200

    handle edit password Add password controller and edit password view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'main#index'
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
+
   get 'sign_up', to: 'registration#new'
   post 'sign_up', to: 'registration#create'
+
   get 'sign_in', to: 'session#new'
   post 'sign_in', to: 'session#create'
+
   delete 'logout', to: 'session#destroy'
+
   get 'about-us', to: 'about#index', as: :about
 end
