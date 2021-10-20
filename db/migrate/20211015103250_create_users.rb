9a06c2307633f4ac2c0497f88645c82717e85d6d
ashraffares <ashraffares090@gmail.com>
2021-10-20 11:19:26 +0200

    update email reset file

--- db/migrate/20211015103250_create_users.rb
+++ db/migrate/20211015103250_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :name
       t.string :email, null: false
       t.string :password_digest
 
